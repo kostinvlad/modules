1938bc2f60608a758a041a4dc8e19a6269e5bcfb
Vladislav Kostin <v1adyank0stin@gmail.com>
2020-07-02 18:23:14 +0400

    redone

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,1 @@
-// TODO: write your code here
-import sum from './basic';
-import {Game, GameSavingData, readGameSaving as loadGame, writeGameSaving as saveGame } from './Game/game'
-
-const game = new Game();
-game.start();
+console.log('app worked')
